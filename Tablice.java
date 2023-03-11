71e876905b59ac6a65e9a274e16b9a0e392a59b3
aleksanderZak7 <olekzak7@gmail.com>
2023-03-11 21:17:21 +0100

    PP0502B - Tablice

---
+++ Tablice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Tablice {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int size = input.nextInt();
+            int[] list = new int[size];
+            int[] reverse_list = new int[size];
+            for(int j=0;j<size;j++)
+            {
+                list[j]=input.nextInt();
+            }
+            for(int number : list)
+            {
+                reverse_list[size-1] = number;
+                size -= 1;
+            }
+            for(int number : reverse_list)
+            {
+                System.out.print(number+ " ");
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
