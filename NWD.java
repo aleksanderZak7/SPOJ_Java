2824f74f2da02e262470609c688a87113eea8e94
aleksanderZak7 <olekzak7@gmail.com>
2023-03-11 21:04:18 +0100

    PP0501A - NWD

---
+++ NWD.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Calculations
+{
+    int NWD(int a,int b)
+    {
+        while(a!=b)
+        {
+            if(a>b)
+                a-=b;
+            else
+                b-=a;
+        }
+        return a;
+    }
+}
+
+public class NWD {
+    public static void main(String[] arg){
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            Calculations Result = new Calculations();
+            System.out.println(Result.NWD(a, b));
+        }
+        input.close();
+    }
+}
