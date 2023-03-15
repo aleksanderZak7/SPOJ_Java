305fd520fc2bd504b9609925294937366bc99230
aleksanderZak7 <olekzak7@gmail.com>
2023-03-15 22:24:58 +0100

    STOS - Stos

---
+++ Stos.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Stos 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] stack = new int[10];
+        int index = -1;
+        while(true)
+        {
+            try
+            {
+                char Symbol = input.next().charAt(0);
+                if(Symbol == '+')
+                {
+                    int Number = input.nextInt();
+                    index++;
+                    if(index >= 10)
+                    {
+                        System.out.println(":(");
+                    }
+                    else
+                    {
+                        stack[index] = Number;
+                        System.out.println(":)");
+                    }
+                }
+                else if(Symbol == '-')
+                {
+                    if(index >= 10)
+                    {
+                        index = 9;
+                    }
+                    if(index == -1)
+                    {
+                        System.out.println(":(");
+                    }
+                    else
+                    {
+                        System.out.println(stack[index]);
+                        index--;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                break;
+            }
+        }
+        input.close();
+    }
+}
