476bfa9e989bcf83368b5dec5ad07c113f9c0512
aleksanderZak7 <olekzak7@gmail.com>
2023-03-15 22:38:31 +0100

    KC001 - Dodawanie liczb całkowitych

--- Dodawanie_liczb_calkowitych.java
+++ Dodawanie_liczb_calkowitych.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-
-public class Dodawanie_liczb_calkowitych {
-    public static void main(String[] args){
+class Main{
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
-        System.out.println(a+b+c);
+        System.out.println(a + b + c);
         input.close();
     }
 }
