ebb5a31cd14d96a0926875289390cf73a2c5ac78
aleksanderZak7 <olekzak7@gmail.com>
2023-03-15 22:39:54 +0100

    PP0502B - Tablice

--- Tablice.java
+++ Tablice.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Tablice {
+class Main
+{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
